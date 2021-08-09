944118beb8b39398d8066c659c52d6a2491843a9
evanilukhin <evanilukhin@gmail.com>
2021-08-09 09:46:48 +0300

    Add comments Fix frontend test

--- soil-service/app/services/humus_balance_calculator.rb
+++ soil-service/app/services/humus_balance_calculator.rb
@@ -1,3 +1,17 @@
+# Calculates a humus balance for the passed field data.
+# Class accepts the structure defined in the ::HumusBalanceCalculatorContract. And returns
+# When it is called without arguments fetches data from the ::FieldService.
+# As a result returns the Result monad https://dry-rb.org/gems/dry-monads/1.3/result/ which can be Success or Failure.
+# If the result is Success value will be an object with attributes field_id and humus_balance
+# Success result can be unwrapped by commands .value! for Success and .failure for Failure
+# @example
+#  result = HumusBalancesCalculator.call(fields_params)
+# or
+#  result = HumusBalancesCalculator.call
+# Unwrap
+#  result.value! if result.success?
+#  or
+#  result.failure if result.failure?
 class HumusBalanceCalculator < ApplicationService
   CONSECUTIVE_MULTIPLIER = 1.3
 
--- soil-service/app/services/humus_balances_calculator.rb
+++ soil-service/app/services/humus_balances_calculator.rb
@@ -1,3 +1,15 @@
+# Class accepts array of params for fields with structure defined in the ::HumusBalanceCalculatorContract or nothing.
+# When it is called without arguments fetches data from the ::FieldService.
+# As a result returns the Result monad https://dry-rb.org/gems/dry-monads/1.3/result/ which can be Success or Failure
+# Success result can be unwrapped by commands .value! for Success and .failure for Failure
+# @example
+#  result = HumusBalancesCalculator.call(fields_params)
+# or
+#  result = HumusBalancesCalculator.call
+# Unwrap
+#  result.value! if result.success?
+#  or
+#  result.failure if result.failure?
 class HumusBalancesCalculator < ApplicationService
   def initialize(fields_params = nil)
     @fields_params =
