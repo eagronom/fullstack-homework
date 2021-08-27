5d94172040a5a9dd588e75efb75474e467833027
MiselAdemi <ademimisel@gmail.com>
2021-08-27 08:46:53 +0200

    Add field controller humus balance action

--- soil-service/app/controllers/fields_controller.rb
+++ soil-service/app/controllers/fields_controller.rb
@@ -1,5 +1,17 @@
 class FieldsController < ActionController::Base
+  skip_before_action :verify_authenticity_token, only: [:humus_balance]
+
   def index
     render json: FieldsService.instance.fetch_fields
+  end
+
+  def humus_balance
+    render json: HumusBalanceService.new(humus_balance_params).perform
+  end
+
+  private
+
+  def humus_balance_params
+    params[:fields]&.any? && params.require(:fields)
   end
 end
